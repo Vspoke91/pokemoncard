71b8cf73917b2db1a5cfd7d26532153b77500d1a
Vspoke91 <victorvjro91@gmail.com>
2023-04-01 19:23:28 -0400

    ADD: Other elements for Pokemon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,10 +11,18 @@ function Card () {
     <div className='Card'>
       <p className='Pokemon_Name'>Pokemon Name</p>
       <p className='Pokemon_Type'>Type</p>
-      <p className='Pokemon_Moves'>Moves</p>
-
-      <button onClick={() => getPokemonImage('clefairy', setImageURL)}>Get New Card</button>
       <img src={imageURL} />
+      <div className='Pokemon_Abilities'>Abilities</div>
+      <div className='Pokemon_Moves'>Moves</div>
+      <div className='Pokemon_Stats'>
+        <p className='Stat_HP'>hp</p>
+        <p className='Stat_Attack'>attack</p>
+        <p className='Stat_Defence'>defence</p>
+        <p className='Stat_Specia-Attack'>special-attack</p>
+        <p className='Stat_Special-defence'>special-defense</p>
+        <p className='Stat_Speed'>speed</p>
+      </div>
+      <button onClick={() => getPokemonImage('clefairy', setImageURL)}>Get New Card</button>
     </div>
   )
 }
