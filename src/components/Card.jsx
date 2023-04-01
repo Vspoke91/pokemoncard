41b8e78b043e369bb50a3a1ffae25a2ff0767218
Vspoke91 <victorvjro91@gmail.com>
2023-04-01 18:01:15 -0400

    ADD: getPokemonImage function

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import './Card.css'
 
+const POKEMON_CARD_URL = 'https://pokeapi.co/api/v2/pokemon'
+
 // getPokemon('bulbasaur')
 function Card () {
   const [imageURL, setImageURL] = useState('')
@@ -10,10 +12,20 @@ function Card () {
 
   return (
     <div className=''>
-
+      <button onClick={() => getPokemonImage('bulbasaur', setImageURL)}>Test</button>
       <img src={imageURL} />
     </div>
   )
 }
 
+function getPokemonImage (pokemonName, setState) {
+  fetch(`${POKEMON_CARD_URL}/${pokemonName}`)
+    .then((res) => res.json())
+    .then((json) => json.sprites)
+    .then((sprites) => sprites.other)
+    .then((other) => other.dream_world)
+    .then((dreamWorld) => {
+      setState(dreamWorld.front_default)
+    })
+}
 export default Card
