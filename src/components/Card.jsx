3b4be73fcb6a7a3156d05b0a7aceaf23fe8dd820
Vspoke91 <victorvjro91@gmail.com>
2023-04-01 20:45:25 -0400

    ADD: methods to fetch API

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,36 +5,82 @@ const POKEMON_CARD_URL = 'https://pokeapi.co/api/v2/pokemon'
 
 // getPokemon('bulbasaur')
 function Card () {
-  const [imageURL, setImageURL] = useState('')
+  const [pokeImgURL, setPokeImgURL] = useState('')
+  const [pokeType, setPokeType] = useState('')
+  const [pokeAbilities, setPokeAbilities] = useState([])
+  const [pokeMoves, setPokeMoves] = useState([])
+  const [pokeStats, setPokeStats] = useState([])
 
+  function getNewCard () {
+    const pokemon = 'clefairy' // TODO: change this to a random id
+    getPokemonImage(pokemon, setPokeImgURL)
+    getPokemonType(pokemon, setPokeType)// TODO: type is an erray make it so it displays an erra in DOM
+    getPokemonAbilities(pokemon, setPokeAbilities)
+    getPokemonMoves(pokemon, setPokeMoves)
+    getPokemonStats(pokemon, setPokeStats)
+  }
+  // {
   return (
     <div className='Card'>
       <p className='Pokemon_Name'>Pokemon Name</p>
-      <p className='Pokemon_Type'>Type</p>
-      <img src={imageURL} />
-      <div className='Pokemon_Abilities'>Abilities</div>
-      <div className='Pokemon_Moves'>Moves</div>
-      <div className='Pokemon_Stats'>
-        <p className='Stat_HP'>hp</p>
-        <p className='Stat_Attack'>attack</p>
-        <p className='Stat_Defence'>defence</p>
-        <p className='Stat_Specia-Attack'>special-attack</p>
-        <p className='Stat_Special-defence'>special-defense</p>
-        <p className='Stat_Speed'>speed</p>
+      <p className='Pokemon_Type'>Type {pokeType}</p>
+      <img src={pokeImgURL} height='100px' width='100px' />
+      <div className='Pokemon_Abilities'>
+        Abilities
+        {
+          pokeAbilities.length &&
+          pokeAbilities.map((ability, index) =>
+            <p key={index}>{ability.ability.name}</p>
+          )
+        }
       </div>
-      <button onClick={() => getPokemonImage('clefairy', setImageURL)}>Get New Card</button>
+      <div className='Pokemon_Moves'>
+        Moves
+        {
+          pokeMoves.length &&
+          pokeMoves.slice(0, 5).map((move, index) =>
+            <p key={index}>{move.move.name}</p>
+          )
+        }
+      </div>
+      <div className='Pokemon_Stats'>
+        {
+          pokeStats.length &&
+          pokeStats.map((stat, index) =>
+            <p key={index}>{stat.stat.name} - {stat.base_stat}</p>
+          )
+        }
+      </div>
+      <button onClick={() => getNewCard()}>Get New Card</button>
     </div>
   )
 }
 
-function getPokemonImage (pokemonName, setImageURL) {
+function getPokemonImage (pokemonName, setState) {
   fetch(`${POKEMON_CARD_URL}/${pokemonName}`)
     .then((res) => res.json())
-    .then((json) => json.sprites)
-    .then((sprites) => sprites.other)
-    .then((other) => other.dream_world)
-    .then((dreamWorld) => {
-      setImageURL(dreamWorld.front_default)
-    })
+    .then((json) => setState(json.sprites.other.dream_world.front_default))
+}
+
+function getPokemonType (pokemonName, setState) {
+  fetch(`${POKEMON_CARD_URL}/${pokemonName}`)
+    .then((res) => res.json())
+    .then((json) => setState(json.types[0].type.name))
+}
+
+function getPokemonAbilities (pokemonName, setState) {
+  fetch(`${POKEMON_CARD_URL}/${pokemonName}`)
+    .then((res) => res.json())
+    .then((json) => setState(json.abilities))
+}
+function getPokemonMoves (pokemonName, setState) {
+  fetch(`${POKEMON_CARD_URL}/${pokemonName}`)
+    .then((res) => res.json())
+    .then((json) => setState(json.moves))
+}
+function getPokemonStats (pokemonName, setState) {
+  fetch(`${POKEMON_CARD_URL}/${pokemonName}`)
+    .then((res) => res.json())
+    .then((json) => setState(json.stats))
 }
 export default Card
