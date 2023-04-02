e4726a3f238bd3312882d8d2531987d5ebf46963
Vspoke91 <victorvjro91@gmail.com>
2023-04-01 23:19:02 -0400

    CHNG: Moves are now ouside card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -21,46 +21,49 @@ function Card () {
     getPokemonStats(pokemon, setPokeStats)
     getPokemonName(pokemon, setPokeName)
   }
-  // {
+
   return (
     <div className='Card'>
-      <p className='Pokemon_Name'>{pokeName}</p>
-      <div className='Pokemon_Type'>
-        <h1>Type</h1>
-        {
-          pokeTypes.length &&
-          pokeTypes.map((type, index) =>
-            <p key={index}>{type.type.name}</p>
-          )
-        }
+      <div className='Main_Card'>
+        <h1 className='Pokemon_Name'>{pokeName}</h1>
+        <div className='Pokemon_Type'>
+          <h1>Type</h1>
+          {
+            pokeTypes.length &&
+            pokeTypes.map((type, index) =>
+              <p key={index}>{type.type.name}</p>
+            )
+         }
+        </div>
+        <img className='Pokemon_Img' src={pokeImgURL} />
+        <div className='Pokemon_Abilities'>
+          <h1>Abilitites</h1>
+          {
+            pokeAbilities.length &&
+            pokeAbilities.map((ability, index) =>
+              <p key={index}>{ability.ability.name}</p>
+            )
+          }
+        </div>
+        <div className='Pokemon_Stats'>
+          <h1>Stats</h1>
+          {
+            pokeStats.length &&
+            pokeStats.map((stat, index) =>
+              <p key={index}>{stat.stat.name} - {stat.base_stat}</p>
+            )
+          }
+        </div>
       </div>
-      <img className='Pokemon_Img' src={pokeImgURL} />
-      <div className='Pokemon_Abilities'>
-        <h1>Abilitites</h1>
-        {
-          pokeAbilities.length &&
-          pokeAbilities.map((ability, index) =>
-            <p key={index}>{ability.ability.name}</p>
-          )
-        }
-      </div>
+
       <div className='Pokemon_Moves'>
         <h1>Moves</h1>
         {
-          pokeMoves.length &&
-          pokeMoves.slice(0, 5).map((move, index) =>
-            <p key={index}>{move.move.name}</p>
-          )
-        }
-      </div>
-      <div className='Pokemon_Stats'>
-        <h1>Stats</h1>
-        {
-          pokeStats.length &&
-          pokeStats.map((stat, index) =>
-            <p key={index}>{stat.stat.name} - {stat.base_stat}</p>
-          )
-        }
+            pokeMoves.length &&
+            pokeMoves.slice(0, 5).map((move, index) =>
+              <p key={index}>{move.move.name}</p>
+            )
+          }
       </div>
       <button className='Get_Pokemon_Button' onClick={() => getNewCard()}>Get Next Card</button>
     </div>
